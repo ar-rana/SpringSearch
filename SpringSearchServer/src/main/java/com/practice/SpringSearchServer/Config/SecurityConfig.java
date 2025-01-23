6e23b24e6c8ce272eee7ba3ce3f4bb876272e2f9
Aryan Rana <aryan.rana2005@gmail.com>
2025-01-23 21:11:19 +0530

    improved prompt

--- SpringSearchServer/src/main/java/com/practice/SpringSearchServer/Config/SecurityConfig.java
+++ SpringSearchServer/src/main/java/com/practice/SpringSearchServer/Config/SecurityConfig.java
@@ -3,13 +3,11 @@ package com.practice.SpringSearchServer.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +20,9 @@ public class SecurityConfig {
                 .cors().and()
                 .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
-                            .requestMatchers("/verify", "/oauth2/**").permitAll()
-                            .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll() // Allow preflight
-                            .anyRequest().authenticated())
+                        .requestMatchers("/verify", "/oauth2/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow preflight
+                        .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .oauth2Login(oauth2 -> oauth2
                         .defaultSuccessUrl("http://localhost:3000/", true));
--- SpringSearchServer/src/main/java/com/practice/SpringSearchServer/Controller.java
+++ SpringSearchServer/src/main/java/com/practice/SpringSearchServer/Controller.java
@@ -2,14 +2,12 @@ package com.practice.SpringSearchServer;
 
 import java.util.List;
 
-import javax.validation.constraints.Null;
-
-import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,6 +22,8 @@ public class Controller {
 
     private ChatClient chatClient;
 
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     public Controller(ChatClient.Builder chatClient) {
         this.chatClient = chatClient.build();
     }
@@ -35,14 +35,21 @@ public class Controller {
 
     @PostMapping("/answerJava")
     public String getAnswer(@RequestBody String text) {
-        System.out.println("question: " + text);
-        String condition = "    (You are being used for a Java encyclopedia(SpringSearch) so you MUST only reply to JAVA Question and just return \"ask me java!!\" otherwise)";
-        String constraint = " keep your response under 250 world ";
-        String ask = text + constraint + condition;
+        String condition = "Your primary function is to respond to java based questions so you must only reply to java focused question and return \"ask me java!!\" otherwise";
+        String constraint = "keep your response under 300 worlds";
+
+        logger.info("User Question: "+ text);
+
+        SystemMessage llmCondition = new SystemMessage(condition);
+        SystemMessage llmConstraint = new SystemMessage(constraint);
+        UserMessage userMessage = new UserMessage(text);
+
+        Prompt ask = new Prompt(List.of(llmCondition, llmConstraint, userMessage));
         try {
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             if (auth != null && auth.isAuthenticated()) {
-                return chatClient.prompt().user(ask).call().content();
+//                return chatClient.prompt().user(text).call().content();
+                return chatClient.prompt(ask).call().content();
             } else {
                 return "UNAUTHORIZED USER";
             }
